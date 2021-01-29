6a8525082e6a1e3d5272ac83616e9371333df5a0
Martin Lecerf <lecerf.mar@orange.fr>
2021-01-29 10:17:18 +0100

    init

---
+++ src/test/java/m2i/spring/recette/RecetteApplicationTests.java
@@ -0,0 +1,13 @@
+package m2i.spring.recette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
